93fa0a07a9d59d1c2a8e8632720df8292fb7c855
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-18 23:18:47 -0400

    added center printing setitngs

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def test(a, b, depth, lowerBound):
 def getCenter(x1, x2, y1, y2, d):
     while True:
         # gets random point
-        e, f = randomInBounds(x1, x2), randomInBounds(y1, y2)
+        e, f = randomInBounds(x1, x2), randomInBounds(y1, y2)       ### IF YOU WANT YOUR OWN CENTER, WRITE "e, f = x, y" WHERE X AND Y ARE THE COORDINATES
 
         # gets a lower bound for the test
         lb = 1.8 if d < 20 else (1.6 if d < 50 else (1.4 if d < 100 else (1.2 if d < 200 else 1)))
@@ -132,3 +132,6 @@ imageio.mimsave("plot.gif", images, duration=0.1)
 # removes files
 for d in range(1, depth+1):
     os.remove(f"imgs/plot{d}.png")
+
+# prints center
+print(f"Your center was ({cx}, {cy})")
